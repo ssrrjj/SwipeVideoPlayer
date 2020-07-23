91d86c72ffd8b8f8b27e04c2cc528e8d8a08a580
RenjieShao <shaorj1219@gmail.com>
2020-07-23 14:33:51 -0700

    import opencv

---
+++ OpenCV.h
@@ -0,0 +1,17 @@
+//
+//  OpenCV.h
+//  SwipeVideoPlayer
+//
+//  Created by Renjie Shao on 7/23/20.
+//  Copyright © 2020 Renjie Shao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OpenCV : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
